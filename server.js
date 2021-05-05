ee06f7ef43d0153c23391dc400872534e7e58bd4
Karolina Okla <karolinaokla95@gmail.com>
2021-05-05 21:32:58 +0200

    Add task and remove task server function

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
+const cors = require('cors');
+
+let tasks = [];
 
 const app = express();
+app.use(cors());
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running...');
+  });
+  
+  app.use((req, res) => {
+    res.status(404).send({ message: 'Not found...' });
+  });
+
+  const io = socket(server);
+  io.on('connection', (socket) => {
+    socket.emit('updateData', tasks);
+    socket.on('addTask', task => {
+      console.log(`${socket.id} adds ${task}`);
+      tasks.push(task);
+      socket.broadcast.emit('addTask', task);
+    });
+    socket.on('removeTask', i => {
+      console.log(`${socket.id} removes task ${i}`);
+      tasks.splice(i, 1);
+      socket.broadcast.emit('removeTask', i);
+    });
+  });
