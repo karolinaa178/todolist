daaffc4deab94af953f0fc7381ec5cd1c5a13896
Karolina Okla <karolinaokla95@gmail.com>
2021-05-06 12:59:26 +0200

    Add tasks functionality

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const socket = require('socket.io');
 const cors = require('cors');
 
@@ -8,25 +7,31 @@ let tasks = [];
 const app = express();
 app.use(cors());
 
-const server = app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running...');
-  });
-  
-  app.use((req, res) => {
-    res.status(404).send({ message: 'Not found...' });
-  });
+app.use('*',(req, res) => {
+  res.send('Not found...');
+});
 
-  const io = socket(server);
-  io.on('connection', (socket) => {
-    socket.emit('updateData', tasks);
-    socket.on('addTask', task => {
-      console.log(`${socket.id} adds ${task}`);
-      tasks.push(task);
-      socket.broadcast.emit('addTask', task);
-    });
-    socket.on('removeTask', i => {
-      console.log(`${socket.id} removes task ${i}`);
-      tasks.splice(i, 1);
-      socket.broadcast.emit('removeTask', i);
-    });
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running...');
+});
+  
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New socket', socket.id);
+  socket.emit('updateData', tasks);
+  socket.on('removeTask', (id) => {
+    tasks = tasks.filter(task => task.id !== id);
+    socket.broadcast.emit('removeTask', id);
   });
+  socket.on('addTask', task => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('updateTask', newTask => {
+    console.log(`${socket.id} updates task ${newTask.id}`);
+    const task = tasks.find(task => task.id === newTask.id);
+    task.name =newTask.name;
+    socket.broadcast.emit('updateTask', newTask);
+  });
+});
